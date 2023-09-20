9dbc1b5f1bb0565e581d2a9263485eda0d69b927
CyanBrown <Ltp#1825656>
2023-09-20 00:03:20 -0500

    retake captured pawns and implement no unecessary pawn move

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,27 +2,46 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net.Security;
 
 public class MyBot : IChessBot
 {
     // target rank variable is the furthest pushed pawn
     public int targetRank = 2;
+    Dictionary<String, Move> tradeMap = new Dictionary<String, Move>();
+    int bestMoveUnsafePawn = 9;
 
     public Move Think(Board board, Timer timer)
     {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        while (true)
+        {
+            //some other processing to do possible
+            if (stopwatch.ElapsedMilliseconds >= 1000)
+            {
+                break;
+            }
+        }
+
         Move[] moves = board.GetLegalMoves();
         PieceList pieces = board.GetPieceList(PieceType.Pawn, true);
         Dictionary<Piece, List<Move>> pawns = GetPawns(moves, pieces);
         // get all pawns and their associated Piece object Dictionary<Piece, List<Move>>
         Move bestMove = moves[0];
-        
-        int bestMoveUnsafePawn = 9;
-        // to find the moves possible for pawns use Move.StartSquare and associate it with Piece.Square
-        // whenever create dictionary we should update the target rank variable
 
-        // loop through pawns if their position is behind target rank then move it or if en passant possible
-        // use target rank - Piece.Square.File % 2 to decide if push pawn
+        String currentFen = board.GetFenString();
+        foreach (KeyValuePair<String, Move> retakeMove in tradeMap)
+        {
+            if(currentFen == retakeMove.Key)
+            {
+                foreach (Move nextMove in moves)
+                {
+                    if(nextMove.Equals(retakeMove.Value))
+                    {
+                        return nextMove;
+                    }
+                }
+            }
+        }
 
         foreach (KeyValuePair<Piece, List<Move>> pawn in pawns)
         {
@@ -31,64 +50,78 @@ public class MyBot : IChessBot
                 continue;
             }
 
-            // Move finalMove;
-            // keep this
             foreach(Move move in pawn.Value)
             {
                 if(move.IsEnPassant)
                 {
                     return move;
                 }
-                
-                int unSafePawns = 0;
-                board.MakeMove(move);
-                
-                foreach(Move oppMove in board.GetLegalMoves(true))
-                {
-                    if (oppMove.CapturePieceType == PieceType.Pawn)
-                    {
-                        board.MakeMove(oppMove);
-                        foreach(Move ourMove in board.GetLegalMoves(true))
-                        {
-                            if (oppMove.TargetSquare != ourMove.TargetSquare)
-                            {
-                                unSafePawns++;
-                            }
-                            
-                            
-                        }
-                        board.UndoMove(oppMove);
-                    }
-                }
-                board.UndoMove(move);
-                Debug.WriteLine(unSafePawns);
-                if (unSafePawns < bestMoveUnsafePawn) 
+
+                if (IsBestMove(board, move))
                 {
                     bestMove = move;
-                    bestMoveUnsafePawn = unSafePawns;
-
                 }
             }
-
-            // check if we did the possible move (may be multiple) would pawns be in danger - makemove
-            // if currently pawns are in danger and moving another only puts more/keeps it in danger then we use other piece to protect - tryskipmove
-            
-            // priority is if we can find a pawn move where no pawns are unprotected then use that move
-            
-
         }
 
-        //if(pawn.Key.Square.Rank < targetRank - ((pawn.Key.Square.File % 2)*2))
-        //{
-        /*            return pawn.Value[0];      
-                }*/
 
-        // then we check for current position and we iterate all the moves and see if there is a situation in which all pawns are protected or more than the best scenario for the pawn moves
+        foreach (Move move in moves)
+        {
+            if(move.MovePieceType == PieceType.Pawn)
+            {
+                continue;
+            }
 
-        // instead we need to decide if we need to protect, recapture, or move in different order
-        // check the skip turn followup moves by black and the planned move and see if it's safe
-        return bestMoveUnsafePawn != 9 ? bestMove : moves[0];
+            if(IsBestMove(board, move))
+            {
+                bestMove = move;
+            }
+        }
 
+        if(bestMoveUnsafePawn == 9)
+        {
+            return moves[0];
+        }
+
+        bestMoveUnsafePawn = 9;
+        return bestMove;
+
+    }
+
+    private bool IsBestMove(Board board, Move move)
+    {
+        int unSafePawns = 0;
+        board.MakeMove(move);
+        foreach (Move oppMove in board.GetLegalMoves(true))
+        {
+            if (oppMove.CapturePieceType == PieceType.Pawn)
+            {
+                board.MakeMove(oppMove);
+                bool canRetake = false;
+                foreach (Move ourMove in board.GetLegalMoves(true))
+                {
+                    if (oppMove.TargetSquare.Equals(ourMove.TargetSquare))
+                    {
+                        if (!tradeMap.ContainsKey(board.GetFenString()))
+                            tradeMap.Add(board.GetFenString(), ourMove);
+                        canRetake = true;
+                    }
+                }
+                if (!canRetake)
+                    unSafePawns++;
+
+                board.UndoMove(oppMove);
+            }
+        }
+        board.UndoMove(move);
+
+        if (unSafePawns < bestMoveUnsafePawn)
+        {
+            bestMoveUnsafePawn = unSafePawns;
+            return true;
+        }
+
+        return false;
     }
 
     private Dictionary<Piece, List<Move>> GetPawns(Move[] moves, PieceList pawns)
